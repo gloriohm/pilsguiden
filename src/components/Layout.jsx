860957476691df4e15a1b59913cd73c0e3d59e02
mangum0pus <138219079+mangum0pus@users.noreply.github.com>
2023-09-08 20:34:07 +0200

    changed colors, added Oslo bars, added Seljor list

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,25 @@
 const barsOslo = [
     {
+        bar: "Nils Bar & Cafe",
+        price: 68,
+    },
+    {
+        bar: "Sagene bar",
+        price: 74,
+    },
+    {
+        bar: "Hersleb grill og bar",
+        price: 77,
+    },
+    {
+        bar: "Luna",
+        price: 84,
+    },
+    {
+        bar: "Cacadou",
+        price: 86,
+    },
+    {
         bar: "Cafe Sara",
         price: 98,
     },
@@ -9,19 +29,43 @@ const barsOslo = [
     }
 ]
 
+const barsSeljord = [
+    {
+        bar: "Fjellheimpuben",
+        price: 95,
+    },
+    {
+        bar: 'Preik',
+        price: 106,
+    }
+]
+
 export default function Layout() {
     return (
         <>
             <h1 className="flex text-xl font-semibold mx-36">Topplista — Oslo</h1>
             <main>
-                {barsOslo.map(({ bar, price }) => (
-                    <ul className="mx-36">
-                        <li className="flex justify-between font-semibold" key={bar}>
-                            <span>{bar}</span>
-                            <span>{price},-</span>
-                        </li>
-                    </ul>
-                ))}
+                <div>
+                    {barsOslo.map(({ bar, price }) => (
+                        <ul className="mx-36">
+                            <li className="flex justify-between font-semibold" key={bar}>
+                                <span>{bar}</span>
+                                <span>{price},-</span>
+                            </li>
+                        </ul>
+                    ))}
+                </div>
+                <h1 className="flex text-xl font-semibold mx-36">Topplista — Seljord</h1>
+                <div>
+                    {barsSeljord.map(({ bar, price }) => (
+                        <ul className="mx-36">
+                            <li className="flex justify-between font-semibold" key={bar}>
+                                <span>{bar}</span>
+                                <span>{price},-</span>
+                            </li>
+                        </ul>
+                    ))}
+                </div>
             </main>
         </>
     )
